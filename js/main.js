10cfd5528f078d9e4fc17e91c67e70648e6373b7
Jennifer Jakobsson <146066161+jenniferchristine@users.noreply.github.com>
2023-10-06 14:58:16 +0200

    Deleted comments

--- js/main.js
+++ js/main.js
@@ -1,97 +1,78 @@
 "use strict";
 
-// Variabler
+let newtodobuttonEl = document.getElementById("newtodobutton");
+let messageEl = document.getElementById("message");
+let newtodoEl = document.getElementById("newtodo");
+let todolistEl = document.getElementById("todolist");
+let clearbuttonEl = document.getElementById("clearbutton");
 
-let newtodobuttonEl = document.getElementById("newtodobutton"); // Lägg-till-knapp
-let messageEl = document.getElementById("message"); // Felmeddelande
-let newtodoEl = document.getElementById("newtodo"); // Box för text
-let todolistEl = document.getElementById("todolist"); //Listan items läggs inuti
-let clearbuttonEl = document.getElementById("clearbutton"); // Återställ-knapp
-
-// Events (för när något ska hända, så som knappar)
-
-clearbuttonEl.addEventListener("click", clearStorage); // Event för clear
-newtodoEl.addEventListener("keyup", checkInput, false); // Event för att stämma av input
-newtodobuttonEl.addEventListener("click", addToDo, false); // Event för att addera till lsita
+clearbuttonEl.addEventListener("click", clearStorage); 
+newtodoEl.addEventListener("keyup", checkInput, false);
+newtodobuttonEl.addEventListener("click", addToDo, false); 
 window.onload = init;
-
-// Väntar på att läsa in data
 
 function init() {
     console.log("Initierar...");
-    newtodobuttonEl.disabled = true; // Knappen funkar ej
+    newtodobuttonEl.disabled = true; 
 
-    loadToDo(); // Läs in att-göra-lista
+    loadToDo(); 
 }
-
-// Stämmer av input
 
 function checkInput() {
     console.log("Input kontrolleras...");
 
     let input = newtodoEl.value;
 
-    if (input.length > 4) { // Villkor för om texten är mer än 4st
+    if (input.length > 4) { 
         messageEl.innerHTML = "";
-        newtodobuttonEl.disabled = false; // = Knappen funkar
+        newtodobuttonEl.disabled = false;
     } else {
-        messageEl.innerHTML = "Minst fem tecken"; // Meddelande om mindre än 4st
-        newtodobuttonEl.disabled = true; // = Knappen funkar ej
+        messageEl.innerHTML = "Minst fem tecken";
+        newtodobuttonEl.disabled = true;
     }
 }
-
-// Lägg till punkt på listan
 
 function addToDo() {
     console.log("Lägger till att-göra...");
 
     let input = newtodoEl.value;
-
-    // För att få nya element
-    let newEl = document.createElement("article"); // Skapa <article>
+    let newEl = document.createElement("article");
     let newTextNode = document.createTextNode(input);
 
     newEl.appendChild(newTextNode);
     newEl.className = "todoItems";
-    todolistEl.appendChild(newEl); // Lägger till i föräldra-element
-
-    newtodoEl.value = ""; // Lägger ej till om inget är skrivet
+    todolistEl.appendChild(newEl); 
+    newtodoEl.value = "";
     newtodobuttonEl.disabled = true;
 
     // Hanterare
-    newEl.addEventListener("click", function (e) { // Tar bort en "att-göra" via klick
+    newEl.addEventListener("click", function (e) {
         e.target.remove();
 
-        saveToDo(); // Lagrar endast borttagningen
+        saveToDo(); 
     });
-    saveToDo(); // Lagrar tillägget
+    saveToDo();
 }
-
-// Spara "att-göra" till Web Storage
 
 function saveToDo() {
     console.log("Lagrar att-göra-lista...");
 
     let todoItem = document.getElementsByClassName("todoItems");
+    let tempArr = [];
 
-    let tempArr = []; // Tom array
-
-    for (let i = 0; i < todoItem.length; i++) { // Loop för att beräkna array
-        tempArr.push(todoItem[i].innerHTML); // Push för lägga i tom array
+    for (let i = 0; i < todoItem.length; i++) {
+        tempArr.push(todoItem[i].innerHTML);
     }
 
-    let jsonStr = JSON.stringify(tempArr); // Konvertera
-    localStorage.setItem("todoItems", jsonStr); // Lagra i Web Storage
+    let jsonStr = JSON.stringify(tempArr);
+    localStorage.setItem("todoItems", jsonStr);
 
-    console.log(tempArr); // Visar arrayena 
+    console.log(tempArr);
 }
-
-// Läs in "att-göra" till Web Storage
 
 function loadToDo() {
     console.log("Läser in att-göra-lista");
 
-    // Läs in och konvertera från JSON till array
     let todoItem = JSON.parse(localStorage.getItem("todoItems"));
 
     if (todoItem != null) {
@@ -105,22 +86,21 @@ function loadToDo() {
 
             todolistEl.appendChild(newEl);
 
-            // Klick-hanterare
             newEl.addEventListener("click", function (e) {
                 e.target.remove();
 
-                saveToDo(); // Lagra listan på nytt
+                saveToDo(); 
             });
             console.log(todoItem);
         }
     }
 }
 
-function clearStorage() { // Funktion för att rensa sidan
-    localStorage.clear(); // Rensa Web Storage
+function clearStorage() {
+    localStorage.clear(); 
 
-    if (localStorage.length === 0) { // Villkor för om webben tömts...
-        todolistEl.innerHTML = ""; // ... Ska också listan göras det direkt
+    if (localStorage.length === 0) { 
+        todolistEl.innerHTML = ""; 
 
         loadToDo();
     }
